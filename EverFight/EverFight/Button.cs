c9a20ff8db917664dd3189f1d54939ecd5ae9859
ppareek19 <ppareek19@sidwell.edu>
2019-04-15 08:45:07 -0400

    Work on button texture

--- EverFight/EverFight/Button.cs
+++ EverFight/EverFight/Button.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,31 @@ namespace EverFight
     class Button
     {
 
-        public Button(Vector2 pos)
+        //Properties
+        Vector2 position;
+        Game1.ButtonType buttonType;
+        Texture2D buttonTexture;
+        Texture2D activeButtonTexture;
+
+        public Button(Vector2 pos, Game1.ButtonType type)
+        {
+            position = pos;
+            buttonType = type;
+
+        }
+
+        public void LoadContent(ContentManager cm)
+        {
+            buttonTexture = cm.Load<Texture2D>('');
+            activeButtonTexture = cm.Load<Texture2D>('');
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
         {
 
         }
